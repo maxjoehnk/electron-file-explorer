7f66a0ab379ed063dd2c9555c9bb7e1339e87eea
Max Jöhnk <maxjoehnk@gmail.com>
2017-08-17 11:22:23 +0200

    fix: fetch items for initial path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MdIconRegistry} from '@angular/material';
+import {Store} from '@ngrx/store';
+import {IState} from './store';
+import {Navigate} from './store/actions/path';
+import {remote} from 'electron';
+const {app} = remote;
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
     viewMode = 'list';
 
-    constructor(private iconRegistry: MdIconRegistry) {
+    constructor(private iconRegistry: MdIconRegistry,
+                private store: Store<IState>) {
         iconRegistry.setDefaultFontSetClass('mdi');
+    }
+
+    ngOnInit(): void {
+        const path = app.getPath('home');
+        this.store.dispatch(new Navigate(path));
     }
 
     changeViewMode(mode) {
--- src/app/store/effects/path.ts
+++ src/app/store/effects/path.ts
@@ -1,29 +1,19 @@
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
-import { Injectable } from '@angular/core';
-import { ipcRenderer, remote } from 'electron';
+import {Injectable} from '@angular/core';
 
-const { app } = remote;
-
-import { Navigate, PATH_NAVIGATE } from '../actions/path';
-import { ITEMS_FETCH } from '../actions/items';
-import { IState } from '../index';
-import {Store} from '@ngrx/store';
+import {Navigate, PATH_NAVIGATE} from '../actions/path';
+import {FetchItems} from '../actions/items';
 import {Actions, Effect} from '@ngrx/effects';
 
 @Injectable()
-export class PathEffects {
+export class PathEffects  {
 
-    constructor(
-        private actions$: Actions,
-        private store: Store<IState>
-    ) {
-        store.dispatch(new Navigate(app.getPath('home')));
-    }
+    constructor(private actions$: Actions) {}
 
     @Effect()
     items$ = this.actions$
         .ofType<Navigate>(PATH_NAVIGATE)
-        .map(({ payload }) => ({ type: ITEMS_FETCH, payload }));
+        .map(({payload}) => new FetchItems(payload));
 
 }
